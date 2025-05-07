62a6dddf259daf38c7db87f65d2d14a8e7c0d11a
om325 <121870809+om325@users.noreply.github.com>
2025-05-08 00:40:31 +0530

    Delete RMIChatApp2 directory

--- RMIChatApp2/ChatService.java
+++
@@ -1,6 +0,0 @@
-import java.rmi.Remote;
-import java.rmi.RemoteException;
-
-public interface ChatService extends Remote {
-    String sendMessage(String clientName, String message) throws RemoteException;
-}
